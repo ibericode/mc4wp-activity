a5af678d1365c015e51690ac60738576b53e3f71
Danny van Kooten <dannyvankooten@gmail.com>
2019-02-15 10:20:44 +0100

    bump tested wp version

--- mc4wp-activity.php
+++ mc4wp-activity.php
@@ -11,7 +11,7 @@ Domain Path: /languages
 License: GPL v2
 
 MailChimp Activity
-Copyright (C) 2015-2017, ibericode <support@ibericode.com>
+Copyright (C) 2015-2019, ibericode <support@ibericode.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
