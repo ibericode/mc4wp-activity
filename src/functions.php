87c7778b0132cbf3aa3e220764b1f836a9f540b7
Danny van Kooten <dannyvankooten@gmail.com>
2019-10-04 08:40:36 +0200

    compat with new list cache in mc4wp core

--- src/functions.php
+++ src/functions.php
@@ -3,22 +3,12 @@
 namespace MC4WP\Activity;
 
 use MC4WP_MailChimp;
-use MC4WP_API_v3;
 
 function ajax_handler() {
     $list_id   = (string) $_GET['mailchimp_list_id'];
     $period  = isset( $_GET['period'] ) ? (int) $_GET['period'] : 30;
-    $options = mc4wp_get_options();
-
-    if( class_exists( 'MC4WP_API_v3' ) ) {
-        $api = new MC4WP_API_v3( $options['api_key'] );
-        $raw_data = $api->get_list_activity( $list_id, array( 'count' => $period ) );
-    } else {
-        // for backwards compatibility, use old API v2 class.
-        require_once __DIR__ . '/API.php';
-        $api       = new API( $options['api_key'] );
-        $raw_data = $api->get_lists_activity( $list_id );
-    }
+    $api = mc4wp_get_api_v3();
+    $raw_data = $api->get_list_activity( $list_id, array( 'count' => $period ) );
 
     if( $_REQUEST['view'] === 'activity' ) {
         require_once __DIR__ . '/ActivityData.php';
@@ -27,7 +17,7 @@ function ajax_handler() {
         require_once __DIR__ . '/SizeData.php';
         $mailchimp = new MC4WP_MailChimp();
         $list = $mailchimp->get_list( $list_id );
-        $data      = new SizeData( $raw_data, $list->subscriber_count, $period );
+        $data      = new SizeData( $raw_data, $list->stats->member_count, $period );
     }
 
     wp_send_json_success( $data->to_array() );
